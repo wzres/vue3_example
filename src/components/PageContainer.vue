ea32feae9af878d2f0b931901bc54f4483b1a94f
wz <3565087505@qq.com>
2024-05-23 15:00:56 +0800

    big-event-template init

---
+++ src/components/PageContainer.vue
@@ -0,0 +1,21 @@
+<template>
+ <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span>{{ title }}</span>
+        <slot name="right"></slot>
+      </div>
+    </template>
+    <!-- 展开的内容部分 -->
+   <!-- 后台管理项目中，这里通常会放表格、表单子类的数据 -->
+   <slot></slot>
+  </el-card>
+</template>
+
+<script setup>
+defineProps(['title'])
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,7 +1,31 @@
 <template>
-    <div>
-        主页布局
-    </div>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+        <el-menu router
+		    active-text-color="#ffd04b" 
+            background-color="teal" 
+            :default-active="$route.path"
+            text-color="#fff"
+          >
+			
+			<!--与多级菜单同级的菜单项-->
+            <el-menu-item index="/article/category">
+                <el-icon><Aim /></el-icon> <span>分类管理</span>
+            </el-menu-item>
+			<!--与多级菜单同级的菜单项-->
+            <el-menu-item index="/article/manage">
+                <el-icon><Aim /></el-icon> <span>文章管理</span>
+            </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header><h4>测试管理系统</h4></el-header>
+        <el-main><router-view></router-view></el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script setup>
@@ -9,5 +33,13 @@
 </script>
 
 <style lang="scss" scoped>
-
+.el-container {
+    height: 100vh;
+}
+.el-aside {
+    background-color: teal;
+}
+.el-menu {
+  border-right:none
+}
 </style>

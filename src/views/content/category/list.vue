f253b50e78397ee5e9c344099b7a078f59aeded0
wz <3565087505@qq.com>
2025-01-25 20:00:51 +0800

    finish

--- src/views/content/category/list.vue
+++ src/views/content/category/list.vue
@@ -21,16 +21,24 @@
   >
   <template #default="{ node, data }">
         <span class="custom-tree-node">
-          <el-input v-if="data.flag" @blur="handleBlur(node,data)" v-model="category.name" placeholder="请输入子分类名，按回车保存" size="small" @keyup.enter="confirm" />
+          <el-input v-if="data.flag" 
+          @blur="handleBlur(node,data)"  
+          v-model="category[data.id]" 
+          placeholder="请输入子分类名，按回车保存" 
+          size="small" @keyup.enter="confirm" />
           <span v-else>{{ node.label }}</span>
           <!-- <span else>{{ node.label }}</span> -->
           <!-- 新增 -->
-          <el-button  style="margin-left: 8px" v-if="node.level <= 1" @click="append(data)" :icon="Plus" type="primary" circle plain size="small" />
+          <el-button  style="margin-left: 8px" 
+          v-if="node.level <= 1" @click="append(node,data)" 
+          :icon="Plus" type="primary" circle plain size="small" />
           
           <el-button  v-if="data.isSave && isLastChild(node,data) "  @click="handleSave($event,node,data)">确定</el-button>   
             
           <!-- 编辑 -->
-          <el-button style="margin-left: 8px" @click="edit(data)"  :icon="Edit" type="primary" circle plain size="small" />
+          <el-button style="margin-left: 8px" 
+          @click="edit(data)"  :icon="Edit" 
+          type="primary" circle plain size="small" />
           <!-- 删除 -->
           <el-popconfirm v-if="data.children === null || data.children.length === 0" @confirm="remove(node, data)" :title="`你确定要删除 ${data.name} 吗`"  width="250px" :icon="WarnTriangleFilled">
             <template #reference>
@@ -53,9 +61,8 @@
 <script setup>
 import {Plus,Delete,WarnTriangleFilled,Edit} from '@element-plus/icons-vue'
 import { addApi, listApi } from '@/api/concategory';
-import { ref } from 'vue';
-import { data } from 'autoprefixer';
-import { ElMessage } from 'element-plus';
+import { reactive, ref } from 'vue';
+import { ElMessage} from 'element-plus';
 
 defineOptions({
     name:'Category'
@@ -63,7 +70,7 @@ defineOptions({
 
 const cateData = ref([])
 
-
+const subData = ref([])
 
 const defaultProps = {
   children: 'children',
@@ -77,32 +84,67 @@ const render = async() => {
 
 render()
 
-const category = ref({
+const category = reactive({
 })
 
-const append = (data) =>{
-    console.log(data)
-    category.value = { name : '', pid: null  }
-    category.value.pid = data.id
-    data.children.push({
-        name:category.value.name,
-        children:null,
-        flag:true,
-        isSave:true
-    })
+const append = (node,data) =>{
+  // 初始化 category 对象
+  node.expanded = true
+  const newId = Date.now()
+
+  // 向当前节点的 children 数组中添加一个新的子节点
+  data.children.push({
+    id: newId,
+    name: '',
+    children: null,
+    flag: true,
+    isSave: false
+  })
+
+  subData.value.push({
+    subId:newId,
+    name:'',
+    pid:data.id
+  })
+
+  category[newId] = ""
 }
 
 const handleBlur = (node,data) => {
-    data.name  =  category.value.name
+/*     console.log(data)
+    console.log(category) */
+
+   /*  if(!category[data.id]){
+        category[data.id] = ''
+    } */
+
     // 非法判断
-    if( !(data.name.trim()) ){
-        node.parent.data.children.pop(1)
-        ElMessage.error('请输入内容')
+    if( category[data.id].trim() === ''){
+        console.log('输入为空')
+    ElMessage.error('请输入内容')
+        // 移除新增的子节点
+        const index = node.parent.data.children.indexOf(data)
+        if (index > -1) {
+        node.parent.data.children.splice(index, 1)
+        }
+
+        delete category[data.id]
+
         return;
     }
 
+      // 更新子分类名称
+    data.name = category[data.id]
+    const subItem = subData.value.find(item => item.subId === data.id)
+    if(subItem) {
+        subItem.name = data.name
+    }
+
+
+  // 隐藏输入框
    setTimeout(() => {
     data.flag = false
+    data.isSave = true
    }, 200);
 
 
@@ -129,12 +171,20 @@ const edit = (data) => {
     console.log(data)
 }
 
-const handleSave = (e,node,data) => {
+const handleSave = async(e,node,data) => {
     data.isSave  = false
     e.stopPropagation()
-    console.log(123)
-    console.log(node,data)
-}
+    /* const newSubData = subData.value.map(item =>{
+        delete item.subId
+        return {...item}
+    }) */
+
+    if(subData.value.length >0){
+      await addApi(subData.value)
+      ElMessage.success('添加成功')
+    }else ElMessage.error('添加失败')
+    
+}   
 
 // 判断当前节点是否为最后一个子节点
 const isLastChild = (node, data) => {
